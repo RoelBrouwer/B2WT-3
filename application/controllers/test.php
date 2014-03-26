0e22f2e9dc9ea32e53b9487d86c41dd6e7bb55b0
Roel Brouwer <roelbrouwer@msn.com>
2014-03-26 18:34:53 +0100

    view en controller voor personality test

---
+++ application/controllers/test.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+	public function index()
+	{
+		//Vul de $data met alle vragen, waarbij vragen bestaan uit een formulering van de vraag, een label voor de vraag en een array met 3 antwoorden. Elk antwoord is ook weer een array van een value en een tekst.
+		
+		//@TODO: De vragen moeten eigenlijk uit de database opgehaald worden in de toekomst
+		$data['questions'] = array('q1' => array('question' => "Tekst voor vraag 1", 'tag' => "q1", 'answers' => array('a' => array('value' => "tag1", 'text' => "Tekst1"), 'b' => array('value' => "tag2", 'text' => "Tekst2"), 'c' => array('value' => "tag3", 'text' => "Tekst3"))));
+		$this->load->view('personality_test', $data);
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/personality_test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>DataDate - Persoonlijkheidstest</title>
+</head>
+<body>
+<div id="container">
+	<h1>De vragen</h1>
+	<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+	<?php foreach ($questions as $q):?>
+		<h4><?php echo $q['question'];?></h4>
+		<?php foreach ($q['answers'] as $answer):?>
+			<input type="radio" name="<?php echo $q['tag'];?>" value="<?php echo $answer['value'];?>" /> <?php echo $answer['text'];?><br />
+		<?php endforeach;?>
+	<?php endforeach;?>
+	<input type="submit" value="Submit">
+	</form>
+</div>
+
+</body>
+</html>
