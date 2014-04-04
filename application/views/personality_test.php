5688d955973c20dca7eab17e0653341f88b0d6d0
Roel Brouwer <roelbrouwer@msn.com>
2014-04-04 10:24:52 +0200

    Form validatie beter

--- application/views/personality_test.php
+++ application/views/personality_test.php
@@ -8,14 +8,14 @@
 <body>
 <div id="container">
 	<!-- Color: #F46867 -->
-	<?php if (isset($debuginfo)) { echo $debuginfo; }
+	<?php echo validation_errors();
 	if (isset($questions)) { ?>
 	<h1>De vragen</h1>
-	<?php echo form_open('test/retrieve_answers');
+	<?php echo form_open('test');
 	foreach ($questions as $q):?>
 		<h4><?php echo $q['text'];?></h4>
 		<?php foreach ($q['answers'] as $answer):
-			echo form_radio(array('name' => $q['tag'], 'value' => $answer['answer_tag'], 'checked' => 'checked'));
+			echo form_radio(array('name' => $q['tag'], 'value' => $answer['answer_tag']));
 			echo $answer['text'];?><br />
 		<?php endforeach;
 	endforeach;
