204bccca195c3386a551229334b7f844cb61e42d
Roel Brouwer <roelbrouwer@msn.com>
2014-04-09 17:54:04 +0200

    Vergaande input-validatie (nog niet volledig af)

--- application/controllers/reg.php
+++ application/controllers/reg.php
@@ -3,13 +3,42 @@ class Reg extends CI_Controller {
 	
 	public function index()
 	{
-		
-		$this->load->view('registration_form', NULL);
+		$this->load->helper(array('form', 'url'));
+		$this->load->library('form_validation');
+		$this->form_validation->set_message('required','U bent vergeten uw %s in te voeren.');
+		$this->form_validation->set_message('max_length','Uw %s is te lang.');
+		$this->form_validation->set_message('min_length','Uw %s is te kort.');
+		$this->form_validation->set_message('alpha_numeric','Gebruik alleen alfanumerieke karakters in %s.');
+		$this->form_validation->set_message('alpha','Gebruik alleen letters in %s.');
+		$this->form_validation->set_message('is_natural','De %s moet een getal zijn.');
+		$this->form_validation->set_message('date_validation','Voer een geldige %s in.');
+		$this->form_validation->set_rules('username', 'gebruikersnaam', 'trim|required|min_length[3]|max_length[30]|is_unique[users.nickname]|alpha_numeric|xss_clean');
+		$this->form_validation->set_rules('password', 'wachtwoord', 'trim|required|matches[password_check]|md5');
+		$this->form_validation->set_rules('password_check', 'wachtwoord een tweede keer', 'trim|required');
+		$this->form_validation->set_rules('email', 'e-mailadres', 'trim|required|valid_email|is_unique[users.email]');
+		$this->form_validation->set_rules('first_name', 'voornaam', 'trim|required|alpha|xss_clean');
+		$this->form_validation->set_rules('last_name', 'achternaam', 'trim|required|alpha|xss_clean');
+		$this->form_validation->set_rules('birthdate', 'geboortedatum', 'required|callback_date_validation|xss_clean');
+		$this->form_validation->set_rules('gender', 'geslacht', 'required');
+		$this->form_validation->set_rules('picture', 'foto', '');
+		$this->form_validation->set_rules('description', 'beschrijving', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('gender_pref', 'geslachtsvoorkeur', 'required');
+		$this->form_validation->set_rules('min_age', 'gewenste minimumleeftijd', 'required|is_natural|less_than[120]');
+		$this->form_validation->set_rules('max_age', 'gewenste maximumleeftijd', 'required|is_natural|less_than[120]');
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('registration_form', NULL);
+		}
+		else
+		{
+		}
 	}
 	
-	function submit_form()
-	{
-		
+	function date_validation($string) {
+		if ( preg_match('/^([0-9]{1,2})-([0-9]{1,2})-([0-9]{4})$/', $string, $match) ) 
+            return ( checkdate($match[2], $match[1], $match[3]) );
+		else
+			return false;
 	}
 }
 ?>
--- application/views/registration_form.php
+++ application/views/registration_form.php
@@ -7,21 +7,22 @@
 </head>
 <body>
 <div id="container">
-	<?php if (isset($debuginfo)) { echo $debuginfo; } ?>
+	<?php echo validation_errors(); ?>
 	<h1>Registratie</h1>
-	<?php echo form_open('reg/submit_form'); ?>
-	Gebruikersnaam: <?php echo form_input(array('name' => 'username', 'maxlength' => '25', 'size' => '30')) ?> <br />
+	<?php echo form_open('reg'); ?>
+	Gebruikersnaam: <?php echo form_input(array('name' => 'username', 'maxlength' => '25', 'size' => '30', 'value' => set_value('username'))) ?> <br />
 	Wachtwoord: <?php echo form_password(array('name' => 'password', 'maxlength' => '25', 'size' => '30')) ?> <br />
 	Herhaal wachtwoord: <?php echo form_password(array('name' => 'password_check', 'maxlength' => '25', 'size' => '30')) ?> <br />
-	E-mail: <?php echo form_input(array('name' => 'email', 'maxlength' => '35', 'size' => '30')) ?> <br />
-	Voornaam: <?php echo form_input(array('name' => 'first_name', 'maxlength' => '20', 'size' => '30')) ?> <br />
-	Achternaam: <?php echo form_input(array('name' => 'last_name', 'maxlength' => '30', 'size' => '30')) ?> <br />
-	Geslacht: <?php echo form_radio(array('name' => 'gender', 'value' => 'M', 'checked' => 'checked')) ?> Man <?php echo form_radio(array('name' => 'gender', 'value' => 'V')) ?> Vrouw <br />
+	E-mail: <?php echo form_input(array('name' => 'email', 'maxlength' => '35', 'size' => '30', 'value' => set_value('email'))) ?> <br />
+	Voornaam: <?php echo form_input(array('name' => 'first_name', 'maxlength' => '20', 'size' => '30', 'value' => set_value('first_name'))) ?> <br />
+	Achternaam: <?php echo form_input(array('name' => 'last_name', 'maxlength' => '30', 'size' => '30', 'value' => set_value('last_name'))) ?> <br />
+	Geboortedatum: <?php echo form_input(array('name' => 'birthdate', 'maxlength' => '10', 'size' => '12', 'value' => set_value('birthdate'))) ?> <br />
+	Geslacht: <?php echo form_radio(array('name' => 'gender', 'value' => 'M', 'checked' => set_radio('gender', 'M'))) ?> Man <?php echo form_radio(array('name' => 'gender', 'value' => 'V', 'checked' => set_radio('gender', 'V'))) ?> Vrouw <br />
 	Foto: <?php echo form_upload(array('name' => 'picture', 'maxlength' => '35', 'size' => '30')) ?> <br />
-	Beschrijving: <?php echo form_textarea(array('name' => 'description', 'rows' => '7', 'cols' => '30')) ?> <br />
+	Beschrijving: <?php echo form_textarea(array('name' => 'description', 'rows' => '7', 'cols' => '30', 'value' => set_value('description'))) ?> <br />
 	<strong>Ik ben op zoek naar</strong> <br />
-	Geslacht: <?php echo form_radio(array('name' => 'gender_pref', 'value' => 'M', 'checked' => 'checked')) ?> Man <?php echo form_radio(array('name' => 'gender_pref', 'value' => 'V')) ?> Vrouw <?php echo form_radio(array('name' => 'gender_pref', 'value' => 'B')) ?> Beide <br />
-	Minimumleeftijd: <?php echo form_input(array('name' => 'min_age', 'maxlength' => '3', 'size' => '5')) ?> Maximumleeftijd: <?php echo form_input(array('name' => 'max_age', 'maxlength' => '3', 'size' => '5')) ?> <br />
+	Geslacht: <?php echo form_radio(array('name' => 'gender_pref', 'value' => 'M', 'checked' => set_radio('gender_pref', 'M'))) ?> Man <?php echo form_radio(array('name' => 'gender_pref', 'value' => 'V', 'checked' => set_radio('gender_pref', 'V'))) ?> Vrouw <?php echo form_radio(array('name' => 'gender_pref', 'value' => 'B', 'checked' => set_radio('gender_pref', 'B'))) ?> Beide <br />
+	Minimumleeftijd: <?php echo form_input(array('name' => 'min_age', 'maxlength' => '3', 'size' => '5', 'value' => set_value('min_age'))) ?> Maximumleeftijd: <?php echo form_input(array('name' => 'max_age', 'maxlength' => '3', 'size' => '5', 'value' => set_value('max_age'))) ?> <br />
 	<?php echo form_submit('reg', 'Submit'); 
 	echo form_close(); ?>
 </div>
