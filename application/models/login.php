c5698e8ffa2cc3280636cd6a194c24a6f39b8a6c
Roel Brouwer <roelbrouwer@msn.com>
2014-04-10 23:15:11 +0200

    temp_users

--- application/models/login.php
+++ application/models/login.php
@@ -41,7 +41,7 @@ class Login extends CI_Model {
 			'key' => $key
 		);
 		
-		$query = $this->db->insert('temp_user',$data);
+		$query = $this->db->insert('temp_users',$data);
 		if ($query)
 		{
 			return true;
@@ -55,7 +55,7 @@ class Login extends CI_Model {
 	public function check_key($key)
 	{
 		$this->db->where('key', $key);
-		$query = $this->db->get('temp_user');
+		$query = $this->db->get('temp_users');
 		
 		if ($query->num_rows() == 1)
 		{
@@ -70,7 +70,7 @@ class Login extends CI_Model {
 	public function add_user($key)
 	{
 		$this->db->where('key', $key);
-		$temp_user = $this->db->get('temp_user');
+		$temp_user = $this->db->get('temp_users');
 		if ($temp_user)
 		{
 			$row = $temp_user->row();
@@ -97,7 +97,7 @@ class Login extends CI_Model {
 			if ($user)
 			{
 				$this->db->where('key', $key);
-				$this->db->delete('temp_user');
+				$this->db->delete('temp_users');
 				return true;
 			}
 			else
