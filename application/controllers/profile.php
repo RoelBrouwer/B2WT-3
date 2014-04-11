7e1c35537abb608006cef783380c2090e72cfda7
Roel Brouwer <roelbrouwer@msn.com>
2014-04-12 00:40:04 +0200

    Paar fixes

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -1,6 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Test extends CI_Controller {
+class Profile extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper(array('form', 'url'));
+		$this->load->library('form_validation');
+	}
 
 	public function index()
 	{
--- application/views/test_succes.php
+++ application/views/test_succes.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>DataDate - Persoonlijkheidstest</title>
-</head>
-<body>
+   	<div class="wrapper">
+	   	<div class="container">
+		    <h3>Uw persoonlijkheidstype is <strong><?php echo $type; ?></strong></h3>
 
-<h3>Uw persoonlijkheidstype is <strong><?php echo $type; ?></strong></h3>
-
-<p> <?php echo $percentage; ?><br />
-<?php echo anchor('', 'Terug naar de homepagina'); ?></p>
-
-</body>
-</html>
+			<p> <?php echo $percentage; ?><br />
+			<?php echo anchor('', 'Terug naar de homepagina'); ?></p>
+		</div>
+	</div>
