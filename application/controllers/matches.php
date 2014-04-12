d8639a2332d2d28a36618717f2e3b6868b360c62
Roel Brouwer <roelbrouwer@msn.com>
2014-04-12 18:24:53 +0200

    Pre-avondeten-commit

--- application/controllers/matches.php
+++ application/controllers/matches.php
@@ -2,4 +2,24 @@
 
 class Matches extends CI_Controller {
 
+	public function index()
+	{
+		if ($this->session->userdata('logged_in'))
+		{
+			$data['user'] = array(1,2,3); //array of users
+			$this->load->view('common/header');
+			$this->load->view('display_matches', $data);
+			$this->load->view('common/footer');
+		}
+		else
+		{
+			redirect('auth');
+		}
+	}
+	
+	public function _matched_users()
+	{
+		$this->some_model->get_users_matching_pref() //Get the users that match the pref
+	}
+
 }
--- application/views/display_matches.php
+++ application/views/display_matches.php
@@ -1,8 +1,8 @@
 <?php $this->load->helper('form','url'); ?>
    	<div class="wrapper">
 	   	<div class="container">
-			U heeft <?php echo count($usr) ?> matches.
-			<?php foreach ($user as $usr)?>
+			U heeft <?php echo count($user) ?> matches.
+			<?php foreach ($user as $usr):?>
 				<div id="userprofile">
 					GEGEVENS
 				</div>
