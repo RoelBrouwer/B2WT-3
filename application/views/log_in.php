93b4ce16744bba7a5e03a7ab3ddbc2d7d7022951
HanQuang <HanQuang@outlook.com>
2014-04-13 01:57:11 +0200

    Styling login

--- application/views/log_in.php
+++ application/views/log_in.php
@@ -10,9 +10,9 @@
 	<div class="container">
 	
 	<h2>Login</h2>
-	<div class="error">
-	<?php echo validation_errors(); ?>
-	</div>
+	<?php if(validation_errors() != false) { 
+		echo '<div class="error">'.validation_errors().'</div>';
+		}?>
 	<div id="login">
 	<?php echo form_open('auth'); ?>
 	<label>Gebruikersnaam: </label><?php echo form_input('username'); ?> <br />
