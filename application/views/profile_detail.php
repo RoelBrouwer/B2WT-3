19a49def41cc0fa44fd3f9d42c191198b7dbbc3f
Han Quang <H.Q.Lu@outlook.com>
2014-04-21 23:26:55 +0200

    Sidebar profile detail

--- application/views/profile_detail.php
+++ application/views/profile_detail.php
@@ -18,22 +18,17 @@
 	   			<div id="profile-name"><p> <?php if($usr_logged_in){echo $firstname." ".$lastname;} else{echo $nickname;}?> </p></div>
 	   			<ul>
 	   				<?php if($usr_logged_in) {
-					if($like == 1 || $like == 2){
-					}  else{
-						echo '<li> Je hebt deze persoon al geliked </li>';
-					}
-
 					if ($like == 4){
-						echo "<li><img src='". base_url() . "assets/images/likes/likes_4.png' alt='Jullie zijn een match!' width='42' height='42'>"; echo "Jullie zijn een match!</li>"; 
+						echo "<li>Jullie zijn een match!</li>"; echo "<li><img src='". base_url() . "assets/images/likes/likes_4.png' alt='Jullie zijn een match!' width='42' height='42'></li>"; 
 					} elseif($like == 3){
-						echo "<li><img src='". base_url() . "assets/images/likes/likes_3.png' alt='Jij liket ". $nickname ." maar ". $nickname ." jou niet' width='42' height='42'>"; echo "Jij liket ". $nickname ." maar ". $nickname ." jou niet.</li>"; 
+						echo "<li>Jij liket ". $nickname ." maar ". $nickname ." jou niet.</li>"; echo "<li><img src='". base_url() . "assets/images/likes/likes_3.png' alt='Jij liket ". $nickname ." maar ". $nickname ." jou niet' width='42' height='42'></li>"; 
 					} elseif($like == 2){
-						echo "<li><img src='". base_url() . "assets/images/likes/likes_2.png' alt='". $nickname ." liket jou, maar jij ". $nickname ." niet' width='42' height='42'>"; echo $nickname ." liket jou, maar jij ". $nickname ." niet.</li>";
+						echo "<li>".$nickname ." liket jou, maar jij ". $nickname ." niet.</li>"; echo "<li><img src='". base_url() . "assets/images/likes/likes_2.png' alt='". $nickname ." liket jou, maar jij ". $nickname ." niet' width='42' height='42'></li>"; 
 					} else{
-						echo '<li><img src="'. base_url() . 'assets/images/likes/likes_1.png" alt="Jullie hebben elkaar niet geliked" width="42" height="42"><a href="'.base_url().'profile/likeuser/'.$user_id.'">Like!</a></li>';
+						echo '<li>Jullie hebben elkaar niet geliked</li><li><img src="'. base_url() . 'assets/images/likes/likes_1.png" alt="Jullie hebben elkaar niet geliked" width="42" height="42"><a href="'.base_url().'profile/likeuser/'.$user_id.'">Like!</a></li>';
 					}
 				} else{
-					echo '<a href="'.base_url().'auth">Leer'.$nickname.'beter kennen..</a>';
+					echo '<a href="'.base_url().'auth">Leer '.$nickname.' beter kennen..</a>';
 				}?>
 	   			</ul>
 	   		</div>
@@ -81,11 +76,6 @@
 					echo "</ul>";
 				}
 				?>
-			</div>
-			<div id="likes">
-				
-
-					
 			</div>
 		</div>
 	</div>
